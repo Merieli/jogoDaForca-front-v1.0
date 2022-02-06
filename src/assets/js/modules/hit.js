813531736e8b983f48408275d5e5078fca7f5bfb
Meriéli Manzano <merieli.dev@gmail.com>
2022-02-06 18:12:25 -0300

    feat(logic): hit part of the quest
    closes issue #5

--- src/assets/js/modules/hit.js
+++ src/assets/js/modules/hit.js
@@ -1,20 +1,13 @@
 
-export function toHit(button, element, list, c) {
-    let count = 0;
-    for(let i in list){        
-        if (list[i] != element){
-            //se errar e adicionado o estilo ao botao e trocado a imagem do Stickyman
-            button.classList.add("inventory-letter--wrong");
-        } else{             
-            //se acertar e adicionado o estilo ao botao e inserido a letra na missao
-            button.classList.add("inventory-letter--hit");
-            let letter = document.querySelector(`[data-quest="${i}"]`);
-            letter.textContent = element;
-            count ++;
-
-            //a cada acerto e acrescido 10 a pontuacao
-        }
+export function hitPoint(button, element, hit){
+    //loop para preencher todos acertos na quest
+    for(let c in hit){
+        let letter = document.querySelector(`[data-quest="${hit[c]}"]`);
+        letter.textContent = element;
     }
+    //adicionar estilo de acerto ao botao
+    button.classList.add("inventory-letter--hit");
+
+    //a cada acerto e acrescido 10 a pontuacao
     
-    return c = c + count;
 }
